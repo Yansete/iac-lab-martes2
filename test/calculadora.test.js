c2b693033287eb6582306f51189482a726e3b1c4
Yansete <yanliudai@gmail.com>
2026-06-30 19:45:46 -0500

    test: agregar prueba para suma con números positivos y negativos

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -6,3 +6,8 @@ import { suma } from '../src/calculadora.js';
 test('TDD: suma dos números positivos', () => {
     assert.equal(suma(1, 1), 2);
 });
+
+test('TDD: suma un número negativo y uno positivo', () => {
+    assert.equal(suma(-1, 1), 0);
+    assert.equal(suma(1, -1), 0);
+});
