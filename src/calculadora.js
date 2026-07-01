9826b79dfedd4557b2ccd9dd88933cdb679ec96f
Yansete <yanliudai@gmail.com>
2026-06-30 19:49:32 -0500

    feat: validar valores numéricos en la suma

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,3 +1,7 @@
 export function suma(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('ERROR: los valores deben ser numéricos');
+    }
+
     return a + b;
 }
